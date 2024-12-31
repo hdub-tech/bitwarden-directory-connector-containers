40fa6fdf399f1d6ce9ea95108d3b096eec68c756
H Dub <14808878+hdub-tech@users.noreply.github.com>
2024-12-31 15:31:41 -0600

    Add args to entrypoint / config working

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,12 @@ BW_ORGUUID=
 usage() {
   cat <<EOM
   USAGE:
-    ${SCRIPT_NAME}
+    ${SCRIPT_NAME} config|test|sync
+
+  Where:
+    * config: Finishes necessary configuration using the secrets provided
+    * test: Does the above config + runs "bwdc test"
+    * sync: Does the above config and test + runs "bwdc sync"
 
   With the following expectations:
     1. bwdc must be on the path
@@ -18,6 +23,7 @@ usage() {
        AND is a supported value (Currently only supports: ${SUPPORTED_BWDC_SYNCS[*]})
     3. BW_CLIENTID and BW_CLIENTSECRET environment variables must be set
     4. If Sync type is gsuite, BW_KEY environment variable must be set
+    5. Only one of the above listed arguments should be provided
 
   Error detected from above list: ${1}
 EOM
@@ -29,33 +35,47 @@ isLoggedOut() {
   [ ! -e "${BW_DATAFILE}" ] || jq -r '.activeUserId == null' "${BW_DATAFILE}"
 }
 
+# login if not already
 login() {
   if isLoggedOut; then
     bwdc login
   fi
 }
 
+# logout if not already
 logout() {
   if ! isLoggedOut; then
     bwdc logout
   fi
 }
 
-# Finish secret set-up so not stored in image
-configBwdc() {
+preReqs() {
+  # Make sure we have bwdc
+  if which bwdc; then
+    BW_DATAFILE="$( bwdc data-file 2>/dev/null )"
   
-  # 'bwdc config' doesn't work when logged out
-  login
+    # Make sure supported Directory Type set in env var
+    if [ -n "${BITWARDENCLI_CONNECTOR_DIRECTORY_TYPE}" ] \
+      && [[ " ${SUPPORTED_BWDC_SYNCS[*]} " =~ [[:space:]]${BITWARDENCLI_CONNECTOR_DIRECTORY_TYPE}[[:space:]] ]]; then
 
-  # The following (as well as the --secretfile version) is confirmed not
-  # working for BITWARDENCLI_CONNECTOR_PLAINTEXT_SECRETS=true mode as it is
-  # adding extra slashes to the newlines for the key
-  bwdc config "${BITWARDENCLI_CONNECTOR_DIRECTORY_TYPE}".key --secretenv BW_KEY
-
-  logout
+      # Make sure Client Id and Secret set in env vars for login
+      if [ -n "${BW_CLIENTID}" ] && [ -n "${BW_CLIENTSECRET}" ]; then
+        BW_ORGUUID="${BW_CLIENTID#organization.}"
+      else
+        usage 3
+      fi
+    else
+      usage 2
+    fi
+  else
+    usage 1
+  fi
 }
 
-configJq() {
+# Finish secret set-up so not stored in image
+config() {
+  preReqs
+
   # bwdc gets angry if you mess with the data.json while logged in
   logout
 
@@ -79,22 +99,17 @@ configJq() {
   echo "${BW_DATAFILE_CONTENTS}" > "${BW_DATAFILE}"
 }
 
-# Make sure we have bwdc
-if which bwdc; then
-  BW_DATAFILE="$( bwdc data-file 2>/dev/null )"
-
-  # Make sure supported Directory Type set in env var
-  if [ -n "${BITWARDENCLI_CONNECTOR_DIRECTORY_TYPE}" ] && [[ " ${SUPPORTED_BWDC_SYNCS[*]} " =~ [[:space:]]${BITWARDENCLI_CONNECTOR_DIRECTORY_TYPE}[[:space:]] ]]; then
-    # Make sure Client Id and Secret set in env vars for login
-    if [ -n "${BW_CLIENTID}" ] && [ -n "${BW_CLIENTSECRET}" ]; then
-      BW_ORGUUID="${BW_CLIENTID#organization.}"
-      configJq
-    else
-      usage 3
-    fi
-  else
-    usage 2
-  fi
+if [ "$#" -ne "1" ]; then
+  usage 5
 else
-  usage 1
+  case "${1}" in
+    "config" )
+      config ;;
+    "test" )
+      echo TODO test ;;
+    "sync" )
+      echo TODO sync ;;
+    "*" )
+      usage 5 ;;
+  esac
 fi
